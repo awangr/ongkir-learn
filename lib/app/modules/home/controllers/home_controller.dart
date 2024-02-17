1dac089188483932b8ef053e33a99041e1184e0d
awang <awangrhomadon@gmail.com>
2024-02-17 09:58:51 +0700

    dd_search

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,21 +1,8 @@
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
+  final hiddenKotaAsal = true.obs;
+  final provAsalId = 0.obs;
+  final hiddenKotaTujuan = true.obs;
+  final provTujuanId = 0.obs;
 }
--- lib/app/modules/home/views/widgets/provinsi.dart
+++ lib/app/modules/home/views/widgets/provinsi.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:cek_ongkir/app/data/model/province_model.dart';
 import 'package:cek_ongkir/app/modules/home/controllers/home_controller.dart';
 import 'package:flutter/material.dart';
@@ -12,9 +14,54 @@ class Provinsi extends GetView<HomeController> {
     return Padding(
       padding: EdgeInsets.only(bottom: 20),
       child: DropdownSearch<Provinces>(
-        clearButtonProps: ClearButtonProps(
-            icon: IconButton(onPressed: () {}, icon: Icon(Icons.cancel))),
-        popupProps: PopupProps.menu(),
+        asyncItems: (String filter) async {
+          Uri url = Uri.parse('https://api.rajaongkir.com/starter/province');
+          try {
+            final res = await http
+                .get(url, headers: {'key': '4cd7712637d772706397b6c436ae3e9e'});
+            var data = jsonDecode(res.body) as Map<String, dynamic>;
+            var statusCode = data['rajaongkir']['status']['code'];
+            if (statusCode != 200) {
+              throw data['rajaongkir']['status']['description'];
+            }
+            var listAllProvince =
+                data['rajaongkir']['results'] as List<dynamic>;
+            var models = Provinces.fromJsonList(listAllProvince);
+            return models;
+          } catch (e) {
+            print('Error + $e');
+            return List<Provinces>.empty();
+          }
+        },
+        onChanged: (value) {
+          if (value != null) {
+            if (tipe == 'asal') {
+              controller.hiddenKotaAsal.value = false;
+              controller.provAsalId.value = int.parse(value.provinceId!);
+            } else {
+              controller.hiddenKotaTujuan.value = false;
+              controller.provTujuanId.value = int.parse(value.provinceId!);
+            }
+          } else {
+            if (tipe == 'asal') {
+              controller.hiddenKotaAsal.value = true;
+              controller.provAsalId.value = 0;
+            } else {
+              controller.hiddenKotaTujuan.value = true;
+              controller.provTujuanId.value = 0;
+            }
+          }
+          print(value);
+        },
+        dropdownDecoratorProps: DropDownDecoratorProps(
+            dropdownSearchDecoration: InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text(
+                    tipe == 'asal' ? 'Provinsi Asal' : 'Provinsi Tujuan'))),
+/*        clearButtonProps: ClearButtonProps(
+            icon: IconButton(onPressed: () {}, icon: Icon(Icons.cancel))),*/
+        popupProps: PopupProps.menu(showSearchBox: true),
+        itemAsString: (item) => item.province!,
       ),
     );
   }
